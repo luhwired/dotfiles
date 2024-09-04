1daf7fa2a6e865be4cd9a86cea04f61bab3e59b1
kaizenluci <154997465+kaizenluci@users.noreply.github.com>
2024-09-04 11:11:09 -0400

    lsp plugins

--- config/neovim/lua/core/plugins.lua
+++ config/neovim/lua/core/plugins.lua
@@ -1,28 +1,63 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+
+if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable", -- Opte por usar a versão estável
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Lazy setup
 require("lazy").setup({
     {
-	"nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
     },
     {
-	"nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" }
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" }
     },
     {
-	"nvim-tree/nvim-tree.lua",
+        "nvim-tree/nvim-tree.lua",
     },
     {
-      'akinsho/toggleterm.nvim', version = "*", config = true
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = true
+    },
+    {
+        "williamboman/mason.nvim",
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright", "tsserver", "lua_ls" },
+            })
+            local lspconfig = require("lspconfig")
+            local servers = { "pyright", "lua_ls", "cmake" }
+
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup({})
+            end
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {noremap = true, silent = true})
+        end
     },
 })
