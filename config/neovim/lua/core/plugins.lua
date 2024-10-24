9bf787ee567e7fc9b90470a3c66211e321c737f5
改善 <bcc8a62d41d282a9fe0ec7ae4c8b57bb@proton.me>
2024-10-23 22:31:30 -0400

    new plugins

--- config/neovim/lua/core/plugins.lua
+++ config/neovim/lua/core/plugins.lua
@@ -15,16 +15,28 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    { "akinsho/bufferline.nvim", version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' } },
+    { "norcalli/nvim-colorizer.lua" },
+    { "numToStr/Comment.nvim" },
     { "nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" } },
     { "nvim-telescope/telescope-ui-select.nvim" },
     { "nvim-tree/nvim-tree.lua" },
     { "akinsho/toggleterm.nvim", version = "*" },
+    { "tpope/vim-fugitive" },
+    { "preservim/vim-markdown", ft = "markdown" },
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim", dependencies = { "neovim/nvim-lspconfig" } },
     { "neovim/nvim-lspconfig" },
-    { "tpope/vim-fugitive" },
-    { "preservim/vim-markdown", ft = "markdown" },
-    { "numToStr/Comment.nvim" },
-    {'norcalli/nvim-colorizer.lua'},
-    {'akinsho/bufferline.nvim', version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' },},
+    { "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+      }
+    },
+    { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "rafamadriz/friendly-snippets" },
 })
+
