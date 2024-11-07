1ecd4390183527d8b7df4423f37c1a81f00f815b
改善 <bcc8a62d41d282a9fe0ec7ae4c8b57bb@proton.me>
2024-11-07 10:29:54 -0400

    new plugins - debug

--- config/neovim/lua/core/plugins.lua
+++ config/neovim/lua/core/plugins.lua
@@ -18,16 +18,23 @@ require("lazy").setup({
     { "akinsho/bufferline.nvim", version = "*", dependencies = { 'nvim-tree/nvim-web-devicons' } },
     { "norcalli/nvim-colorizer.lua" },
     { "numToStr/Comment.nvim" },
+
     { "nvim-telescope/telescope.nvim", tag = "0.1.6", dependencies = { "nvim-lua/plenary.nvim" } },
     { "nvim-telescope/telescope-ui-select.nvim" },
+
     { "nvim-tree/nvim-tree.lua" },
+
     { "akinsho/toggleterm.nvim", version = "*" },
+
     { "tpope/vim-fugitive" },
+
     { "preservim/vim-markdown", ft = "markdown" },
+
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim", dependencies = { "neovim/nvim-lspconfig" } },
     { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp",
+
+    { "hrsh7th/nvim-cmp", 
       dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
@@ -35,7 +42,51 @@ require("lazy").setup({
         "hrsh7th/cmp-cmdline",
       }
     },
+
     { "L3MON4D3/LuaSnip", version = "v2.*", build = "make install_jsregexp" },
     { "saadparwaiz1/cmp_luasnip" },
     { "rafamadriz/friendly-snippets" },
+
+    { "lervag/vimtex", ft = "tex" },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      config = function()
+        require'nvim-treesitter.configs'.setup {
+          highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+          },
+          ensure_installed = { "latex" },
+        }
+      end,
+    },
+
+    {
+	"mfussenegger/nvim-dap",
+	config = function()
+	    require("plugins.nvim_dap")
+	end,
+    },
+    {
+	"nvim-neotest/nvim-nio",
+    },
+
+    {
+	"rcarriga/nvim-dap-ui",
+	requires = {"mfussenegger/nvim-dap"},
+	config = function()
+	    local dap, dapui = require("dap"), require("dapui")
+	    dapui.setup()
+	    dap.listeners.after.event_initialized["dapui_config"] = function()
+		dapui.open()
+	    end
+	    dap.listeners.before.event_terminated["dapui_config"] = function()
+		dapui.close()
+	    end
+	    dap.listeners.before.event_exited["dapui_config"] = function()
+		dapui.close()
+	    end
+	end
+    }
 })
